9e1e9a4e61f4cc431b3f740d8e8fd4abe42da8b5
tuengominh <tue.ngominh@gmail.com>
2018-11-21 00:50:00 +0100

    create Example class

--- src/main/java/tech/bts/cardgame/Example.java
+++ src/main/java/tech/bts/cardgame/Example.java
@@ -36,21 +36,6 @@ public class Example {
         //3 - create battle function
         //with a loop to execute nextBattle() & battle() while deck.deckSize() > 10
 
-        int deckSize = d.deckSize();
-        System.out.println(deckSize);
-        System.out.println(g.getPlayerHand("John").handSize());
-        System.out.println(g.getPlayerHand("Peter").handSize());
-
-        g.battle("John", "Peter");
-        System.out.println(deckSize);
-
-        while(deckSize > Game.MINIMUM_DECK_SIZE){
-            g.nextBattle();
-            g.battle("john", "peter");
-        }
-
-        System.out.println(deckSize);
-
 
     }
 }
