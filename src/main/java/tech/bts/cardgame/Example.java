6e5cfb0794710bb462c16b0dc4f1d2d3a12f1181
tuengominh <tue.ngominh@gmail.com>
2018-11-21 09:01:36 +0100

    try to create Example class

--- src/main/java/tech/bts/cardgame/Example.java
+++ src/main/java/tech/bts/cardgame/Example.java
@@ -9,33 +9,14 @@ public class Example {
 
         //1 - create prepareGame function
         Deck d = new Deck().generate().shuffle();
-        int deckSize = d.deckSize();
-        Game g = prepareGame(d);
 
-        //2 - create pickingCards function
-
-        pickingCards(g);
-
-        //3 - create battle function
-        //with a loop to execute battles while deck.deckSize() > 10
-
-        g.battle("John", "Peter");
-
-        while (deckSize > Game.MINIMUM_DECK_SIZE) {
-            g.nextBattle();
-        }
-
-    }
-
-    public static Game prepareGame(Deck d){
+        System.out.println(d.deckSize());
 
         Game g = new Game(d);
         g.join("John"); //join based on user's input
         g.join("Peter"); //join based on user's input
-        return g;
-    }
 
-    public static void pickingCards(Game g) {
+        //2 - create pickingCards function
 
         g.pickCard("John");
         g.keep("John"); //keep or discard based on user's input
@@ -54,6 +35,24 @@ public class Example {
 
         g.pickCard("Peter");
         g.discard("Peter"); //keep or discard based on user's input
+
+        g.fillHand("Peter");
+
+        System.out.println(g.getPlayerHand("Peter").handSize());
+        System.out.println(g.getPlayerHand("John").handSize());
+        System.out.println(d.deckSize());
+
+        //3 - create battle function
+        //with a loop to execute battles while deck.deckSize() > 10
+
+        g.battle("John", "Peter");
+
+        System.out.println(g.getBattlePoint("John"));
+        System.out.println(g.getBattlePoint("Peter"));
+
+        while (d.deckSize() > Game.MINIMUM_DECK_SIZE) {
+            g.nextBattle();
+        }
 
     }
 }
