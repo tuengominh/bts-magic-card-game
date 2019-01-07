5a7baf9a982431315daaa4876f3449c094e917fb
tuengominh <tue.ngominh@gmail.com>
2019-01-07 10:18:39 +0100

    Create index.html + main.js

--- src/main/resources/static/main.js
+++ src/main/resources/static/main.js
@@ -26,12 +26,33 @@
 
 //let result = sum(4,6);
 //console.log(result);
-
 //function sum(a, b) {return a + b;}
 
 //let array = [2,4,6];
+//let array2 = [];
 //array.push(8);
 //console.log(array[1]);
 //console.log(array);
 //for (let i = 0; i <= array.length; i++) {console.log(array[i]);}
 //for (let value of array) {console.log(value);}
+
+//let car1 = {
+//    name: "Ferrari",
+//    color: "red",
+//    maxSpeed: 300,
+//    available: true};
+
+//let car1 = {
+//    name: "Ford",
+//    maxSpeed: 300,
+//    available: false,
+//    owner: "Tue"};
+
+//the value "null" in Java can be used in operations, "undefined" in JS cannot
+
+//car2.color = "black"; //add properties to an object
+//console.log(car2.color);
+//console.log(car1[color]);
+//let prop = "color";
+//console.log(car1[prop]);
+
