87f8dbc08d1ab39950e2e9ae61fff73df461bf30
tuengominh <tue.ngominh@gmail.com>
2019-01-07 10:01:51 +0100

    Create index.html + main.js

---
+++ src/main/resources/static/main.js
@@ -0,0 +1,37 @@
+//var message = "hello.js"; //var is the old syntax
+//let school = "BTS";
+
+//console.log("hello " + school + " students!");
+//console.log('hello $(school) students');
+
+//let age = 20;
+
+//if (age >= 18) {
+//    console.log("adult");
+//} else {
+//    console.log("not adult");
+//}
+
+//let i = 1;
+//while (i <= 5) {
+//    console.log(i);
+//    i++;}
+
+//for (let j = 1; j <= 5; j++) {
+//    console.log(j);
+//}
+
+//greeting("Tue");
+//function greeting(name) {console.log("hello " + name);}
+
+//let result = sum(4,6);
+//console.log(result);
+
+//function sum(a, b) {return a + b;}
+
+//let array = [2,4,6];
+//array.push(8);
+//console.log(array[1]);
+//console.log(array);
+//for (let i = 0; i <= array.length; i++) {console.log(array[i]);}
+//for (let value of array) {console.log(value);}
