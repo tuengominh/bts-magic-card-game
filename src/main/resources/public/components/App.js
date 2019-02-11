9526580d1aa603c1eb70c9454e295e7da2a18ece
tuengominh <tue.ngominh@gmail.com>
2019-02-11 09:57:34 +0100

    Create React components

---
+++ src/main/resources/public/components/App.js
@@ -0,0 +1,12 @@
+/**
+ * Example component that displays another component (Title).
+ *
+ * Notice every React component needs to have one root element;
+ * that's why we put everything in a <div>.
+ */
+const App = (props) => (
+    <div>
+        <Title text="Hello React" />
+        <p>A simple React example</p>
+    </div>
+);
---
+++ src/main/resources/public/components/Title.js
@@ -0,0 +1,7 @@
+/**
+ * Simple React component that displays a title.
+ * It receives a property `title`.
+ */
+const Title = (props) => (
+    <h1>{props.text}</h1>
+);
